219330efd72b4cda4f4e3f209fb0ad2a810dad47
mazenbitar <36713207+mazenbitar@users.noreply.github.com>
2018-02-28 23:13:23 +0200

    Initial commit

---
+++ app/src/main/java/com/clothing/clothing/RegAct.kt
@@ -0,0 +1,12 @@
+package com.clothing.clothing
+
+import android.support.v7.app.AppCompatActivity
+import android.os.Bundle
+
+class RegAct : AppCompatActivity() {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_reg)
+    }
+}
---
+++ app/src/main/java/com/clothing/clothing/StoreAct.kt
@@ -0,0 +1,12 @@
+package com.clothing.clothing
+
+import android.support.v7.app.AppCompatActivity
+import android.os.Bundle
+
+class StoreAct : AppCompatActivity() {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_store)
+    }
+}
