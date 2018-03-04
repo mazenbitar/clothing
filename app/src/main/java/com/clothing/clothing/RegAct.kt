9ee0b14c600014c934670d1122c13f6b2e67b502
abdallah.sarayrah <abdallah.er11@gmail.com>
2018-03-04 16:49:23 +0200

    area spinner

--- app/src/main/java/com/clothing/clothing/RegAct.kt
+++ app/src/main/java/com/clothing/clothing/RegAct.kt
@@ -20,16 +20,37 @@ class RegAct : AppCompatActivity() {
         setContentView(R.layout.activity_reg)
 
         //bellow i'll file the governorate spinner from the database
-        val url = "http://192.168.0.29/clothing/prl/governorates_select.php"
-        val rq = Volley.newRequestQueue(this)
+        var url = "http://192.168.0.29/clothing/prl/governorates_select.php"
+        var rq = Volley.newRequestQueue(this)
         //this hashmap to store the id and the name of the governorate from the DB
         val hashMapGovernorates = HashMap<Int, String>()
-        val rt = JsonArrayRequest(Request.Method.GET, url, null,
+        var rt = JsonArrayRequest(Request.Method.GET, url, null,
                 Response.Listener { response ->
                     for (index in 0 until response.length()) {
                         hashMapGovernorates[response.getJSONObject(index)
                                 .getInt("governorate_id")] = response.getJSONObject(index)
                                 .getString("governorate_name")
+                    }
+                    val array = hashMapGovernorates.values.toTypedArray()
+                    val adapter = ArrayAdapter(this, android.R.layout.simple_spinner_item,
+                            array)
+                    adapter.setDropDownViewResource(android.R.layout.simple_dropdown_item_1line)
+                    spinner_governorate.adapter = adapter
+                }, Response.ErrorListener { error ->
+            Toast.makeText(this, error.message, Toast.LENGTH_SHORT).show()
+        })
+        rq.add(rt)
+
+        //bellow i'll file the area spinner from the database
+        url = "http://192.168.0.29/clothing/prl/areas_select.php"
+        //this hashmap to store the id and the name of the governorate from the DB
+        val hashMapAreas = HashMap<Int, String>()
+        rt = JsonArrayRequest(Request.Method.GET, url, null,
+                Response.Listener { response ->
+                    for (index in 0 until response.length()) {
+                        hashMapAreas[response.getJSONObject(index)
+                                .getInt("area_id")] = response.getJSONObject(index)
+                                .getString("area_name")
                     }
                     val array = hashMapGovernorates.values.toTypedArray()
                     val adapter = ArrayAdapter(this, android.R.layout.simple_spinner_item,
