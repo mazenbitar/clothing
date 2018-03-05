15aeb358c5a945afa103d30552bcb2cccfb88d80
abdallah.sarayrah <abdallah.er11@gmail.com>
2018-03-05 15:01:32 +0200

    test activity spinner code

--- app/src/main/java/com/clothing/clothing/TestActivity.kt
+++ app/src/main/java/com/clothing/clothing/TestActivity.kt
@@ -1,26 +1,51 @@
 package com.clothing.clothing
 
+import android.annotation.SuppressLint
 import android.support.v7.app.AppCompatActivity
 import android.os.Bundle
+import android.widget.ArrayAdapter
 import android.widget.Toast
 import com.android.volley.Request
 import com.android.volley.Response
 import com.android.volley.toolbox.JsonArrayRequest
 import com.android.volley.toolbox.Volley
+import kotlinx.android.synthetic.main.activity_items.*
+import kotlinx.android.synthetic.main.activity_reg.*
+import kotlinx.android.synthetic.main.activity_test.*
+import org.json.JSONObject
+import java.util.*
 
 class TestActivity : AppCompatActivity() {
 
+    @SuppressLint("SetTextI18n", "UseSparseArrays")
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_test)
 
+        val hashMapTest = HashMap<Int, String>()
         val url = "http://10.152.204.117/clothing/prl/governorates_select.php"
         val rq = Volley.newRequestQueue(this)
         val rt = JsonArrayRequest(Request.Method.GET, url, null,
                 Response.Listener { response ->
-                    Toast.makeText(this, response.getJSONArray(0).toString(),
-                            Toast.LENGTH_LONG).show()
+                    for (i in 0 until response.length()) {
+                        hashMapTest[response.getJSONObject(i).getInt("governorate_id")] =
+                                response.getJSONObject(i).getString("governorate_name")
+                    }
 
-                }, Response.ErrorListener { })
+                    //here i set the first value to appear in the spinner
+                    hashMapTest[0] = "العاصمة"
+
+                    //here i sort the hashmap by keys ascending
+                    val sortedMap = TreeMap(hashMapTest)
+                    val array = sortedMap.values.toTypedArray()
+                    val adapter = ArrayAdapter(this, android.R.layout.simple_spinner_item,
+                            array)
+                    adapter.setDropDownViewResource(android.R.layout.simple_dropdown_item_1line)
+                    spinner3.adapter = adapter
+                }, Response.ErrorListener { error ->
+            Toast.makeText(this, error.message,
+                    Toast.LENGTH_LONG).show()
+        })
+        rq.add(rt)
     }
 }
