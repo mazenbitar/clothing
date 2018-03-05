ec15311314b428dd8b95bd68a6c388bf3dc481c4
abdallah.sarayrah <abdallah.er11@gmail.com>
2018-03-05 13:23:50 +0200

    test jsonarray

--- app/src/main/java/com/clothing/clothing/TestActivity.kt
+++ app/src/main/java/com/clothing/clothing/TestActivity.kt
@@ -2,6 +2,11 @@ package com.clothing.clothing
 
 import android.support.v7.app.AppCompatActivity
 import android.os.Bundle
+import android.widget.Toast
+import com.android.volley.Request
+import com.android.volley.Response
+import com.android.volley.toolbox.JsonArrayRequest
+import com.android.volley.toolbox.Volley
 
 class TestActivity : AppCompatActivity() {
 
@@ -9,6 +14,13 @@ class TestActivity : AppCompatActivity() {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_test)
 
-        val url = ""
+        val url = "http://10.152.204.117/clothing/prl/governorates_select.php"
+        val rq = Volley.newRequestQueue(this)
+        val rt = JsonArrayRequest(Request.Method.GET, url, null,
+                Response.Listener { response ->
+                    Toast.makeText(this, response.getJSONArray(0).toString(),
+                            Toast.LENGTH_LONG).show()
+
+                }, Response.ErrorListener { })
     }
 }
