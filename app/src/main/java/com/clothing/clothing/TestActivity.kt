e64fe1a948ffc1a4d9adf9a0bf2d828d43ae67c8
abdallah.sarayrah <abdallah.er11@gmail.com>
2018-03-06 21:16:35 +0200

    commit

--- app/src/main/java/com/clothing/clothing/TestActivity.kt
+++ app/src/main/java/com/clothing/clothing/TestActivity.kt
@@ -1,7 +1,9 @@
 package com.clothing.clothing
 
 import android.annotation.SuppressLint
+import android.content.Intent
 import android.os.Bundle
+import android.provider.MediaStore
 import android.support.v7.app.AppCompatActivity
 import android.view.View
 import android.widget.AdapterView
@@ -22,41 +24,15 @@ class TestActivity : AppCompatActivity() {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_test)
 
-        val hashMapTest = HashMap<Int, String>()
-        //here i set the first value to appear in the spinner
-        hashMapTest[0] = "العاصمة"
-        val url = "http://10.152.204.117/clothing/prl/governorates_select.php"
-        val rq = Volley.newRequestQueue(this)
-        val rt = JsonArrayRequest(Request.Method.GET, url, null,
-                Response.Listener { response ->
-                    for (i in 0 until response.length()) {
-                        hashMapTest[response.getJSONObject(i).getInt("governorate_id")] =
-                                response.getJSONObject(i).getString("governorate_name")
-                    }
-                    //here i sort the hashmap by keys ascending
-                    val map = TreeMap(hashMapTest)
-                    val array = map.values.toTypedArray()
-                    val adapter = ArrayAdapter(this, android.R.layout.simple_spinner_item,
-                            array)
-                    adapter.setDropDownViewResource(android.R.layout.simple_dropdown_item_1line)
-                    spinner3.adapter = adapter
-                }, Response.ErrorListener { error ->
-            Toast.makeText(this, error.message,
-                    Toast.LENGTH_LONG).show()
-        })
-        rq.add(rt)
-
-        spinner3.onItemSelectedListener = object : AdapterView.OnItemSelectedListener {
-            override fun onItemSelected(parent: AdapterView<*>, view: View, position: Int,
-                                        id: Long) {
-                Toast.makeText(this@TestActivity,
-                        hashMapTest.keys.toTypedArray().sortedArray()[position].toString(),
-                        Toast.LENGTH_SHORT).show()
-            }
-
-            override fun onNothingSelected(parent: AdapterView<*>) {
-                // sometimes you need nothing here
-            }
+        floatingActionButton.setOnClickListener {
+            val intent = Intent(MediaStore.ACTION_IMAGE_CAPTURE)
+            startActivityForResult(intent, 1)
         }
+    }
+
+    override fun onActivityResult(requestCode: Int, resultCode: Int, data: Intent?) {
+        super.onActivityResult(requestCode, resultCode, data)
+
+
     }
 }
