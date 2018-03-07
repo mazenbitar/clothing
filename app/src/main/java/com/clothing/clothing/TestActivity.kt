0c836431a5d2a1ac8dbc4d049ff68e375e777fce
abdallah.sarayrah <abdallah.er11@gmail.com>
2018-03-07 10:30:44 +0200

    commit

--- app/src/main/java/com/clothing/clothing/TestActivity.kt
+++ app/src/main/java/com/clothing/clothing/TestActivity.kt
@@ -2,11 +2,14 @@ package com.clothing.clothing
 
 import android.annotation.SuppressLint
 import android.content.Intent
-import android.graphics.Bitmap
 import android.os.Bundle
 import android.provider.MediaStore
 import android.support.v7.app.AppCompatActivity
 import kotlinx.android.synthetic.main.activity_test.*
+import android.graphics.Bitmap
+import android.R.attr.data
+import android.app.PendingIntent.getActivity
+
 
 class TestActivity : AppCompatActivity() {
 
@@ -16,19 +19,19 @@ class TestActivity : AppCompatActivity() {
         setContentView(R.layout.activity_test)
 
         floatingActionButton.setOnClickListener {
-            /*val intent = Intent(MediaStore.ACTION_IMAGE_CAPTURE)
-            startActivityForResult(intent, 1)*/
-
-            intent = Intent(Intent.ACTION_PICK,
-                    android.provider.MediaStore.Images.Media.EXTERNAL_CONTENT_URI)
-            startActivityForResult(intent, 1)
-
+            val pickPictureIntent = Intent(Intent.ACTION_PICK,
+                    MediaStore.Images.Media.EXTERNAL_CONTENT_URI)
+            startActivityForResult(pickPictureIntent, 1)
         }
     }
 
     override fun onActivityResult(requestCode: Int, resultCode: Int, data: Intent?) {
         super.onActivityResult(requestCode, resultCode, data)
 
-        if (requestCode == 1) imageView7.setImageURI(data?.data)
+        if (requestCode == 1) {
+            val selectedImage = data?.data
+            val bitmap = MediaStore.Images.Media.getBitmap(this.contentResolver, selectedImage)
+            imageView7.setImageBitmap(bitmap)
+        }
     }
 }
